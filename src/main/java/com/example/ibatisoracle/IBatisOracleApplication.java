cc0500e92967e1c93856f75a2852997d7a8b0ec4
Emin Developer <emin.f.mammadov@gmail.com>
2021-12-28 11:09:30 -0800

    Create project about Connect Oracle database using myBatis framework.

---
+++ src/main/java/com/example/ibatisoracle/IBatisOracleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibatisoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBatisOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IBatisOracleApplication.class, args);
+    }
+
+}
